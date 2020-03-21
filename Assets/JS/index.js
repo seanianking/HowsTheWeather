3477a7885e609be6c4330ae80337fd7296056a27
seanianking <seanianking@gmail.com>
2020-03-21 15:38:05 -0600

    reformatted from storing JSON info in a variable to assigning to IDs with Jquery. started UV index call, still working lat and lon into api call. Styled main response info

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,29 +1,44 @@
 var weather = weather;
 
-
+var apikey = "22a6eb6c0d59d0b9ac34a811b19c54e6";
 
 function getTheCurrentWeather() {
     //  API route, pulls city information from the user's input on the web page, then "trims" it by eliminating and spaces in front or behind of the text. Also converts units to imperial system.  //
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $("#pickedCity").val().trim() + "&mode=JSON&units=imperial&APPID=22a6eb6c0d59d0b9ac34a811b19c54e6";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+$("#pickedCity").val().trim()+"&mode=JSON&units=imperial&APPID="+apikey;
     console.log(queryURL);
-    //  Allow async using ajax method // 
+    //  Allow async using 'ajax' method and 'then' method // 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        //  callback to set the returned data to a JSON object
-        weather = {
-            Name: response.name,
-            WeatherIcon: response.weather.icon,
-            WeatherDescription: response.weather[0].description,
-            Temperature: response.main.temp,
-            Humidity: response.main.humidity,
-            Windspeed: response.wind.speed,
-        }
-        console.log(weather);
-        //Callback function to display the weather
-        showCurrentWeather()
-    })
+            url: queryURL,
+            method: "GET"
+        })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function(response) {
+            // Log the resulting object
+            console.log(response);
+
+            // Transfer content to HTML
+            $("#searchedCity").text(response.name);
+            $('#weatherIcon').attr('src', `https://openweathermap.org/img/w/${response.weather[0].icon}.png`)
+            $('#weatherIcon').attr('alt', `${response.weather[0].description}`)
+            $('#weatherType').text(`${response.weather[0].description}`)
+            $("#currentTemp").text("Temperature: " + Math.round(response.main.temp) + " °F");
+            $("#currentHumid").text("Humidity: " + response.main.humidity + '%');
+            $("#currentWind").text("Wind Speed: " + Math.round(response.wind.speed) + 'MPH');
+
+
+            //UV Index API call and appending functionality
+            var queryURLuv = `https://api.openweathermap.org/data/2.5/uvi/forecast?appid=`+apikey+`&lat=`+response.coord.lat+`&lon=`+response.coord.lon+`&cnt=1`
+            $.ajax({
+                    url: queryURLuv,
+                    method: "GET"
+                })
+                .then(function(response) {
+                    $('#currentUv').text("UV Index: " + response[0].value)
+                    console.log(response)
+                })
+        }).fail(function() {
+            // alert('City not found. Try again, please!');
+        });
 };
 
 $("#submit").click(function(event) {
@@ -32,14 +47,24 @@ $("#submit").click(function(event) {
 
 });
 
-function showCurrentWeather() {
-    $("#currentWeather").empty();
-    $.each(weather, function(key, value) {
-        var current = $("<p class='info has-text-weight-semibold'></p>");
-        $("#currentWeather").append(current);
-        $("#currentWeather").append(current.text = (key + " : " + value));
-        console.log(key);
-        console.log(value);
-        console.log(current);
-    })
-}
+
+
+
+
+
+
+
+
+
+//////////*********This function was used initially when the JSON information was set as a variable */
+// function showCurrentWeather() {
+//     $("#currentWeather").empty();
+//     $.each(weather, function(key, value) {
+//         var current = $("<p class='info has-text-weight-semibold'></p>");
+//         $("#currentWeather").append(current);
+//         $("#currentWeather").append(current.text = (key + ": " + value));
+//         console.log(key);
+//         console.log(value);
+//         console.log(current);
+//     })
+// }
