22873374a2a36912b88c7050dda8619d1ad1eafd
seanianking <seanianking@gmail.com>
2020-03-21 06:18:45 -0600

    added script tags for jquery library and linking the js file

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,7 +2,7 @@ var weather = weather;
 
 
 
-function getTheWeather() {
+function getTheCurrentWeather() {
     //  API route, pulls city information from the user's input on the web page, then "trims" it by eliminating and spaces in front or behind of the text. Also converts units to imperial system.  //
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $("#pickedCity").val().trim() + "&mode=JSON&units=imperial&APPID=22a6eb6c0d59d0b9ac34a811b19c54e6";
     console.log(queryURL);
@@ -25,8 +25,21 @@ function getTheWeather() {
     })
 };
 
-$("#submit").click(function() {
-    preventDefault();
-    // getTheWeather();
+$("#submit").click(function(event) {
+    event.preventDefault();
+    // getTheCurrentWeather();
+    console.log(event)
     alert("you clicked me")
-})
+});
+
+function showCurrentWeather() {
+    $("#currentWeather").empty();
+    $.each(weather, function(key, value) {
+        var current = $("<p class='info'></p>");
+        $("#currentWeather").append(current);
+        $("#currentWeather").append(current.text = (key + " : " + value));
+        console.log(key);
+        console.log(value);
+        console.log(current);
+    })
+}
